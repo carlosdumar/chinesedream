a78468b5f762a054e9a71cee51547b1a63049030
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2019-09-09 22:43:11 -0500

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 const title = 'My Simple Express React Webpack Babel Setup Environment';
 
 ReactDOM.render(
-    <div>{title}</div>,
+    <div>
+        <h1>Hola China</h1>
+        <img src="./src/images/china_landscape.jpeg"></img>
+    </div>,
     document.getElementById('app')
 )
